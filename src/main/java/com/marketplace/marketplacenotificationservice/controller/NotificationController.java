918ea32d3fe39ebfbb430a048e4eaf62b8a6f8a3
laleabdullayeva <abdullayevala91@gmail.ru>
2025-12-20 10:36:30 +0400

    initial commit

--- src/main/java/com/marketplace/marketplacenotificationservice/controller/NotificationController.java
+++ src/main/java/com/marketplace/marketplacenotificationservice/controller/NotificationController.java
@@ -5,11 +5,9 @@ import com.marketplace.marketplacenotificationservice.service.NotificationServic
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/notifications")
 public class NotificationController {
@@ -19,7 +17,7 @@ public class NotificationController {
         this.notificationService = notificationService;
     }
 
-    @PostMapping("/email")
+    @PostMapping("/send")
     public ResponseEntity<String> sendEmail(
             @Valid @RequestBody NotificationRequest request
     ) {
